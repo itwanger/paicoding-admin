23708414f27c80633997dc9efd206754df2b9f3e
itwanger <www.qing_gee@163.com>
2023-06-03 09:33:58 +0800

    Update index.tsx

--- src/views/config/index.tsx
+++ src/views/config/index.tsx
@@ -3,7 +3,23 @@
 import { FC, useCallback, useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { DeleteOutlined, EditOutlined, EyeOutlined } from "@ant-design/icons";
-import { Button, Descriptions, Drawer, Form, Image, Input, InputNumber, message, Modal, Select, Space, Switch, Table, Tag,UploadFile } from "antd";
+import {
+	Button,
+	Descriptions,
+	Drawer,
+	Form,
+	Image,
+	Input,
+	InputNumber,
+	message,
+	Modal,
+	Select,
+	Space,
+	Switch,
+	Table,
+	Tag,
+	UploadFile
+} from "antd";
 import type { ColumnsType } from "antd/es/table";
 
 import { delConfigApi, getConfigListApi, operateConfigApi, updateConfigApi } from "@/api/modules/config";
@@ -16,13 +32,13 @@ import Search from "./components/search";
 
 import "./index.scss";
 
-import 'antd/es/modal/style';
-import 'antd/es/slider/style';
+import "antd/es/modal/style";
+import "antd/es/slider/style";
 
 interface DataType {
 	bannerUrl: string;
 	jumpUrl: string;
-	id: number,
+	id: number;
 	key: string;
 	name: string;
 	tags: string;
@@ -30,8 +46,7 @@ interface DataType {
 	status: number;
 }
 
-interface IProps {
-}
+interface IProps {}
 
 // 编辑新增表单的值类型
 export interface IFormType {
@@ -66,7 +81,7 @@ const defaultInitForm: IFormType = {
 // 查询表单默认值
 const defaultSearchForm: ISearchFormType = {
 	type: -1,
-	name: "",
+	name: ""
 };
 
 const Banner: FC<IProps> = props => {
@@ -137,7 +152,7 @@ const Banner: FC<IProps> = props => {
 
 	// 点击取消关闭按钮时触发，关闭抽屉
 	const handleClose = () => {
-		setIsDrawerOpen(false);
+		setIsOpenDrawerShow(false);
 	};
 
 	// 重置表单
@@ -153,7 +168,7 @@ const Banner: FC<IProps> = props => {
 		setIsDrawerOpen(true);
 		// 图片也清空
 		setCoverList([]);
-	}
+	};
 
 	// 删除
 	const handleDel = (configId: number) => {
@@ -187,25 +202,25 @@ const Banner: FC<IProps> = props => {
 		// 如果类型为教程和公告时，详细描述必填
 		if (typeNumber === 4 || typeNumber === 5) {
 			if (!values.content) {
-				message.error("类型为" + ConfigType[type] +"时详细描述不能为空");
+				message.error("类型为" + ConfigType[type] + "时详细描述不能为空");
 				return;
 			}
 		}
 
 		console.log("bannerUrl", bannerUrl);
-		
+
 		// 如果类型为教程和 PDF 时，图片不能为空
 		if (typeNumber === 5 || typeNumber === 6) {
 			if (!bannerUrl) {
-				message.error("类型为" + ConfigType[type] +"时图片不能为空");
+				message.error("类型为" + ConfigType[type] + "时图片不能为空");
 				return;
 			}
 		}
 
 		// 重写新的值
 		const newValues = {
-			...values, 
-			bannerUrl: bannerUrl,// 图片要重新赋值，因为 values 中没有
+			...values,
+			bannerUrl: bannerUrl, // 图片要重新赋值，因为 values 中没有
 			configId: status === UpdateEnum.Save ? UpdateEnum.Save : configId
 		};
 
@@ -235,10 +250,10 @@ const Banner: FC<IProps> = props => {
 	// 数据请求
 	useEffect(() => {
 		const getConfigList = async () => {
-			const { status, result } = await getConfigListApi({ 
+			const { status, result } = await getConfigListApi({
 				...searchForm,
-				pageNumber: current, 
-				pageSize 
+				pageNumber: current,
+				pageSize
 			});
 			const { code } = status || {};
 			//@ts-ignore
@@ -260,12 +275,12 @@ const Banner: FC<IProps> = props => {
 			key: "name",
 			width: 400,
 			render(name, item) {
-				return <span>
-					<Tag  color="orange">
-						{ArticleTag[item.tags]}
-					</Tag>
-					{name}
-				</span>;
+				return (
+					<span>
+						<Tag color="orange">{ArticleTag[item.tags]}</Tag>
+						{name}
+					</span>
+				);
 			}
 		},
 		{
@@ -288,9 +303,8 @@ const Banner: FC<IProps> = props => {
 						onChange={() => {
 							const pushStatus = status === 0 ? 1 : 0;
 							handleOperate(item.id, pushStatus);
-						}
-					}
-				/>
+						}}
+					/>
 				);
 			}
 		},
@@ -328,7 +342,7 @@ const Banner: FC<IProps> = props => {
 								setStatus(UpdateEnum.Edit);
 								const { bannerUrl } = item;
 								console.log("点击编辑 bannerUrl", bannerUrl);
-								handleChange({ configId: id, bannerUrl: bannerUrl});
+								handleChange({ configId: id, bannerUrl: bannerUrl });
 								// 需要设置 image 的值
 								const coverUrl = getCompleteUrl(bannerUrl);
 								// 更新 coverList
@@ -357,13 +371,7 @@ const Banner: FC<IProps> = props => {
 
 	// 编辑表单
 	const reviseDrawerContent = (
-		<Form 
-			name="basic" 
-			form={formRef} 
-			labelCol={{ span: 4 }} 
-			wrapperCol={{ span: 16 }} 
-			autoComplete="off"
-			>
+		<Form name="basic" form={formRef} labelCol={{ span: 4 }} wrapperCol={{ span: 16 }} autoComplete="off">
 			<Form.Item label="类型" name="type" rules={[{ required: true, message: "请选择类型!" }]}>
 				<Select
 					allowClear
@@ -381,11 +389,7 @@ const Banner: FC<IProps> = props => {
 					}}
 				/>
 			</Form.Item>
-			<Form.Item 
-				label="详细描述" 
-				name="content" 
-				tooltip="对公告和教程类型的配置进行一些简短介绍，不要太多字哦"
-				>
+			<Form.Item label="详细描述" name="content" tooltip="对公告和教程类型的配置进行一些简短介绍，不要太多字哦">
 				<Input.TextArea
 					allowClear
 					maxLength={120}
@@ -394,20 +398,15 @@ const Banner: FC<IProps> = props => {
 					}}
 				/>
 			</Form.Item>
-			<Form.Item 
-				label="图片"
-				name="bannerUrl"
-				tooltip="类型为教程和PDF需要，建议尺寸：70*100"
-				>
-				<ImgUpload 
-					coverList={coverList} 
-					setCoverList={setCoverList} 
-					handleChange={handleChange}
-					/>
+			<Form.Item label="图片" name="bannerUrl" tooltip="类型为教程和PDF需要，建议尺寸：70*100">
+				<ImgUpload coverList={coverList} setCoverList={setCoverList} handleChange={handleChange} />
 			</Form.Item>
-			<Form.Item 
+			<Form.Item
 				tooltip="可以是内部或者外部链接"
-				label="跳转URL" name="jumpUrl" rules={[{ required: true, message: "请输入跳转URL!" }]}>
+				label="跳转URL"
+				name="jumpUrl"
+				rules={[{ required: true, message: "请输入跳转URL!" }]}
+			>
 				<Input.TextArea
 					allowClear
 					onChange={e => {
@@ -416,9 +415,12 @@ const Banner: FC<IProps> = props => {
 				/>
 			</Form.Item>
 
-			<Form.Item 
+			<Form.Item
 				tooltip="在用户端显示的时候会用到"
-				label="标签" name="tags" rules={[{ required: false, message: "请选择标签!" }]}>
+				label="标签"
+				name="tags"
+				rules={[{ required: false, message: "请选择标签!" }]}
+			>
 				<Select
 					allowClear
 					onChange={value => {
@@ -442,26 +444,19 @@ const Banner: FC<IProps> = props => {
 		<div>
 			<ContentWrap className="container">
 				{/* 搜索 */}
-				<Search 
+				<Search
 					ConfigTypeList={ConfigTypeList}
 					handleSearchChange={handleSearchChange}
 					handleSearch={handleSearch}
-					handleAdd={handleAdd} />
+					handleAdd={handleAdd}
+				/>
 				{/* 表格 */}
 				<ContentInterWrap>
-					<Table 
-						columns={columns} 
-						dataSource={tableData} 
-						pagination={paginationInfo} />
+					<Table columns={columns} dataSource={tableData} pagination={paginationInfo} />
 				</ContentInterWrap>
 			</ContentWrap>
 			{/* 抽屉 */}
-			<Drawer 
-				title="详情" 
-				placement="right"
-				width={500}
-				onClose={handleClose}
-				open={isOpenDrawerShow}>
+			<Drawer title="详情" placement="right" width={500} onClose={handleClose} open={isOpenDrawerShow}>
 				<Descriptions column={1} labelStyle={{ width: "100px" }}>
 					{detailInfo.map(({ label, title }) => (
 						<Descriptions.Item label={label} key={label}>
@@ -470,47 +465,35 @@ const Banner: FC<IProps> = props => {
 					))}
 					{/* 标签显示 */}
 					<Descriptions.Item label="标签" key="tags">
-						<Tag color="orange">
-							{ArticleTag[tags]}
-						</Tag>
+						<Tag color="orange">{ArticleTag[tags]}</Tag>
 					</Descriptions.Item>
 					{/* 链接显示 */}
 					<Descriptions.Item label="跳转URL" key="jumpUrl">
-						<Button 
-							type="link" 
-							style={{ padding: 0, margin: 0}}
-							target="_blank"
-							href={jumpUrl}
-							>
+						<Button type="link" style={{ padding: 0, margin: 0 }} target="_blank" href={jumpUrl}>
 							{jumpUrl}
 						</Button>
 					</Descriptions.Item>
 					{/* 图片组件显示 */}
 					<Descriptions.Item label="图片" key="bannerUrl">
-						<Image
-							width={100}
-							src={getCompleteUrl(bannerUrl)}
-						/>
+						<Image width={100} src={getCompleteUrl(bannerUrl)} />
 					</Descriptions.Item>
-					
 				</Descriptions>
-				
 			</Drawer>
 			{/* 弹窗 */}
-			<Drawer 
-				title="添加/修改" 
-				open={isDrawerOpen} 
+			<Drawer
+				title="添加/修改"
+				open={isDrawerOpen}
 				width={620}
 				onClose={handleClose}
 				extra={
-          <Space>
-            <Button onClick={handleClose}>取消</Button>
-            <Button type="primary" onClick={handleSubmit}>
-              确定
-            </Button>
-          </Space>
-        }
-				>
+					<Space>
+						<Button onClick={handleClose}>取消</Button>
+						<Button type="primary" onClick={handleSubmit}>
+							确定
+						</Button>
+					</Space>
+				}
+			>
 				{reviseDrawerContent}
 			</Drawer>
 		</div>
