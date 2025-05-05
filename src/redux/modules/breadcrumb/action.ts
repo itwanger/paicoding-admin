20151a62e2bda6b3ba1feee4298b9d37707f2874
jungleford <jungleford@gmail.com>
2025-05-05 18:21:39 +0800

    refactor: ♻️   redux部分重构：Connect API替换RTK之后，移除原有的actions/reducers，以及redux包依赖

--- src/redux/modules/breadcrumb/action.ts
+++
@@ -1,7 +0,0 @@
-import * as types from "@/redux/mutation-types";
-
-// * setBreadcrumbList
-export const setBreadcrumbList = (breadcrumbList: { [propName: string]: any }) => ({
-	type: types.SET_BREADCRUMB_LIST,
-	breadcrumbList
-});
